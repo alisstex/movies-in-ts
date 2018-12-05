1e0a7d061201ff5bfd13fe146240b2ae4fcbfa3e
Alisson Teixeira <alit@fb.com>
2018-12-04 22:48:40 -0800

    add input

--- src/MoviesPage.tsx
+++ src/MoviesPage.tsx
@@ -5,27 +5,63 @@ import Square from "./Square";
 import movies from "./Movies";
 
 class MoviesPage extends React.Component {
-  state = { adding: false };
+  state = { adding: false, movies: movies, inputText: "" };
 
-  addButton = (<button className="moviesAddButton" onClick={(event) => this.addMovie(event)}>Add...</button>);
-  addInput = (<div>blah</div>);
-  getAddingComponent = () => this.state.adding == true ? this.addInput : this.addButton;
+  addButton = (
+    <button
+      className="moviesAddButton"
+      onClick={event => {
+        global.console.log("click add");
+        this.setState({ ...this.state, adding: true });
+      }}
+    >
+      Add...
+    </button>
+  );
+
+  addInput = (
+    <div>
+      <input
+        id="title_input"
+        onChange={evt =>
+          this.setState({ ...this.state, inputText: evt.target.value })
+        }
+      />
+      <button
+        onClick={event => {
+          let newMovies = this.state.movies;
+          newMovies.unshift({
+            title: this.state.inputText,
+            desc: "",
+            link: "",
+            rating: 0
+          });
+          this.setState({ adding: false, movies: newMovies });
+        }}
+      >
+        OK
+      </button>
+    </div>
+  );
+
+  getAddingComponent = () =>
+    this.state.adding ? this.addInput : this.addButton;
 
   public render() {
     return (
-    <div className="App">
-      {this.getAddingComponent()}
-      <div className="moviesgrid">
-        {movies.map((m: MovieEntry) => (
-          <Square key={m.title} movie={m} />
-        ))}
+      <div className="App">
+        {this.getAddingComponent()}
+        <div className="moviesgrid">
+          {this.state.movies.map((m: MovieEntry) => (
+            <Square key={m.title} movie={m} />
+          ))}
+        </div>
       </div>
-    </div>
     );
   }
 
-  addMovie(event: React.MouseEvent<HTMLButtonElement> ) {
-    global.console.log('here');
+  addMovie(event: React.MouseEvent<HTMLButtonElement>) {
+    global.console.log("here");
     this.setState({ adding: true });
   }
 }
